cec57b70d04d542d5f90f3613c243c0c5db94780
Andriikryl <andreyk557@gmail.com>
2023-12-15 15:23:43 +0200

    add hero component

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -3,11 +3,12 @@ import styles from "./style.module.css";
 import { Container } from "../container/Container";
 import Button from "../button/Button";
 import SocialGroup from "../socialGroup/SocialGroup";
+import Image from "next/image";
 
 export default function Hero() {
   return (
     <section className={styles.hero}>
-      <Container>
+      <Container className={styles.hero__container}>
         <div className={styles.hero__box}>
           <SocialGroup />
           <div className={styles.hero__text}>
@@ -19,7 +20,10 @@ export default function Hero() {
               delivers a refreshing burst of flavor and nicotine in a clean,
               more discreet, smokeless way
             </p>
-            <Button text="Read more"></Button>
+            <Button text="Read more" />
+          </div>
+          <div className={styles.image__box}>
+            <Image src="/hero/hero.png" width={500} height={500} alt="" />
           </div>
         </div>
       </Container>
